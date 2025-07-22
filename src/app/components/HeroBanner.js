38b7bf3bc9badbb7aec4095af20c757c7de6560a
Prakul Gupta <pgupta6_be22@thapar.edu>
2025-07-22 20:59:54 +0530

    header

--- src/app/components/HeroBanner.js
+++ src/app/components/HeroBanner.js
@@ -21,7 +21,7 @@ const HeroBanner = () => {
       {/* Background */}
       <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=&quot;60&quot; height=&quot;60&quot; viewBox=&quot;0 0 60 60&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot;%3E%3Cg fill=&quot;none&quot; fill-rule=&quot;evenodd&quot;%3E%3Cg fill=&quot;%239C92AC&quot; fill-opacity=&quot;0.05&quot;%3E%3Ccircle cx=&quot;30&quot; cy=&quot;30&quot; r=&quot;1&quot;/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
 
-      <div className="relative z-10 flex items-center justify-center min-h-screen px-8">
+      <div className="relative z-10 flex items-center justify-center min-h-screen px-8 pt-32 md:pt-36 lg:pt-40">
         <div className="max-w-6xl mx-auto">
           <div className="text-center space-y-12">
 
